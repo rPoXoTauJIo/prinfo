1f97ebe501d2cde1cfbaba0c7ca1345183e80e94
rpoxo <irpoxotaujio@gmail.com>
2019-03-31 21:59:33 +0300

    added speed

--- prinfo.py
+++ prinfo.py
@@ -8,43 +8,114 @@
 #
 # -------------------------------------------------------------------------
 
-# importing python system modules
-import sys
 import time
-import math
 
-# importing modules from standart bf2 package
 import bf2
 import host
 
-# importing project reality packages
+import game.realityadmin as radmin
 import game.realitytimer as rtimer
 
-G_TRACKED_OBJECT = None
-G_UPDATE_TIMER = None
-# last onUpdate call
-G_UPDATE_LAST = 0.0
+from math3d import Point3
 
 # ------------------------------------------------------------------------
 # Init
 # ------------------------------------------------------------------------
 def init():
-    host.registerGameStatusHandler(onGameStatusChanged)
+    WatchVehicle.init()
+    #Speed.init()
+    #AoA.init()
 
-# ------------------------------------------------------------------------
-# DeInit
-# ------------------------------------------------------------------------
-def deinit():
-    host.unregisterGameStatusHandler(onGameStatusChanged)
+class WatchVehicle:
+    reporting = False
+    mode = 'position' # default
+    timer = None
 
+    vehicle = None
+    vehicle_lastposition = (0.0, 0.0, 0.0)
+    vehicle_lastrotation = (0.0, 0.0, 0.0)
+    vehicle_lasttime = 0.0
 
-# ------------------------------------------------------------------------
-# onGameStatusChanged
-# ------------------------------------------------------------------------
-def onGameStatusChanged(status):
-    if status == bf2.GameStatus.Playing:
-        host.registerHandler('EnterVehicle', onEnterVehicle)
-        host.registerHandler('ExitVehicle', onExitVehicle)
+    @classmethod
+    def init(cls):
+        host.registerHandler('EnterVehicle', cls.onEnterVehicle)
+        host.registerHandler('ExitVehicle', cls.onExitVehicle)
+        radmin.addCommand("watch", cls.switchReporting, 777)
+        radmin.addCommand("position", cls.switchReportPosition, 777)
+        radmin.addCommand("rotation", cls.switchReportRotation, 777)
+        radmin.addCommand("speed", cls.switchReportSpeed, 777)
+    
+    @classmethod
+    def switchReporting(cls, args, player):
+        if cls.reporting: cls._disableReporting()
+        else: cls._enableReporting(player.getVehicle())
+        
+    @classmethod
+    def switchReportPosition(cls, args, player):
+        cls.mode = 'position'
+    
+    @classmethod
+    def switchReportRotation(cls, args, player):
+        cls.mode = 'rotation'
+    
+    @classmethod
+    def switchReportSpeed(cls, args, player):
+        cls.mode = 'speed'
+
+    @classmethod
+    def onEnterVehicle(cls, player, vehicle, freeSoldier=False):
+        cls.vehicle = vehicle
+        debugIngame('%s entered %s' % (player.getName(), vehicle.templateName))
+
+    @classmethod
+    def onExitVehicle(cls, player, vehicle):
+        cls._disableReporting()
+        debugIngame('%s exited %s' % (player.getName(), vehicle.templateName))
+
+    @classmethod
+    def _enableReporting(cls, vehicle):
+        cls.timer = rtimer.Timer(cls._tick, -1, 1)
+        cls.timer.setRecurring(0.01)
+    
+    @classmethod
+    def _disableReporting(cls):
+        if cls.timer:
+            cls.timer.destroy()
+            cls.timer = None
+        if cls.vehicle:
+            cls.vehicle
+    
+    @classmethod
+    def _tick(cls, data):
+        epoch = host.timer_getWallTime()
+        position = cls.vehicle.getPosition()
+        rotation = cls.vehicle.getRotation()
+
+        if cls.mode == 'position':
+            cls._position(cls.vehicle, epoch, Point3(*position))
+        elif cls.mode == 'rotation':
+            cls._rotation(cls.vehicle, epoch, rotation)
+        elif cls.mode == 'speed':
+            delta = epoch - cls.vehicle_lasttime
+            cls._speed(cls.vehicle, epoch, delta, Point3(*position), Point3(*cls.vehicle_lastposition))
+    
+    @classmethod
+    def _position(cls, vehicle, epoch, position):
+        debugIngame('%s @%.3f position:(%.3f, %.3f, %.3f)' % (vehicle.templateName, epoch, position.x, position.y, position.z))
+    
+    @classmethod
+    def _rotation(cls, vehicle, epoch, rotation):
+        debugIngame('%s @%.3f rotation:(%.3f, %.3f, %.3f)' % (vehicle.templateName, epoch, rotation[0], rotation[1], rotation[2]))
+
+    @classmethod
+    def _speed(cls, vehicle, epoch, delta, position1, position2):
+        distance = Point3.Distance(position1, position2)
+        speed = distance / delta
+        debugIngame('%s @%.3f-%.3f: %.4f' % (vehicle.templateName, epoch, delta, speed))
+    
+    @classmethod
+    def _angleOfAttack(cls, vehicle, epoch, position0, position1, rotation1, delta):
+        pass
 
 def debugMessage(msg):
     host.rcon_invoke('echo "%s"' % (str(msg)))
@@ -56,48 +127,8 @@ def debugIngame(msg):
     except:
         host.rcon_invoke('echo "debugIngame(FAIL): %s"' % (str(msg)))
 
-
-# 30+-5fps = ~0.33...ms is server frame, no need for updates more frequently than 0.05
-def resetUpdateTimer():
-    global G_UPDATE_TIMER
-
-    if G_UPDATE_TIMER is not None:
-        G_UPDATE_TIMER.destroy()
-        G_UPDATE_TIMER = None
-
-        G_UPDATE_TIMER = rtimer.Timer(onUpdate, 1, 1)
-        G_UPDATE_TIMER.setRecurring(0.1)
-    else:
-        G_UPDATE_TIMER = rtimer.Timer(onUpdate, 1, 1)
-        G_UPDATE_TIMER.setRecurring(0.1)
-    
-    debugMessage('resetUpdateTimer(): reloaded updated timer')
-
-def onUpdate(data=''):
-    global G_UPDATE_LAST
-
-    time_wall_now = host.timer_getWallTime()
-    G_UPDATE_LAST = host.timer_getWallTime()
-
-    infoRaw(G_TRACKED_OBJECT, time_wall_now)
-
-def onEnterVehicle(player, vehicle, freeSoldier=False):
-    global G_TRACKED_OBJECT
-
-    G_TRACKED_OBJECT = vehicle
-    debugMessage('Player entered %s' % (G_TRACKED_OBJECT.templateName))
-    resetUpdateTimer()
-
-
-def onExitVehicle(player, vehicle):
-    global G_TRACKED_OBJECT
-
-    G_TRACKED_OBJECT = None
-    debugMessage('Player exited %s' % (vehicle.templateName))
-    resetUpdateTimer()
-
-
-def infoRaw(vehicle, epoch):
+def infoRaw(vehicle):
+    epoch = host.timer_getWallTime()
     if vehicle is not None and vehicle.isValid():
         position = vehicle.getPosition()
         rotation = vehicle.getRotation()
@@ -106,3 +137,11 @@ def infoRaw(vehicle, epoch):
                                                                                             position[0], position[1], position[2],
                                                                                             rotation[0], rotation[1], rotation[2]
                                                                                             ))
+
+def infoAoA(vehicle):
+    pass
+
+def infoAcceleration(vehicle):
+    pass
+
+
